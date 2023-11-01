6225ed4157472e8a69cfc421edaeb0fdb6f6eb7b
lavina ramchandani <lavinaramchandani99@gmail.com>
2023-11-01 18:56:57 +0530

    add new file

---
+++ es.js
@@ -0,0 +1,80 @@
+
+
+        //  var a=7
+        // console.log(a--) 
+        // console.log(a++)
+        // console.log(a++)
+        // console.log(a) 
+        // console.log(a--)
+        // console.log(a--)
+        // console.log(a++) 
+        // console.log(++a)
+        // console.log(a)
+         
+        // a="12"
+        // b=12
+        // // console.log(a+=b)
+        // // console.log(a*=b)
+        // // console.log(a/=b)
+        // console.log(a==b)
+        // console.log(a<b)
+        // console.log(a>b)
+        // console.log(a<=b)
+        // console.log(a<=b)
+        // console.log(a===b)
+        // a=15
+        // b=16
+        // c=17
+        // d=15
+        // console.log(a==b&&b===c&&c===a&&a==d)
+        // a=10
+        // b=15
+        // c=a
+        // a=b
+        // b=c
+        // console.log(a)
+        // console.log(b)
+        // age=28
+        // age>18?console.log("you can vote"):console.log("you cant't vote");
+        // x="jai";
+        // y="vijay";
+        // console.log('${x}*${y}')\
+        // console.log(x+"*"*y)
+
+        // console.log(typeof(a))
+        // a=5
+        // console.log(typeof(a))
+        // var a
+        // console.log(a)
+        
+
+
+
+
+    
+
+
+
+
+
+
+            
+            
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        
+        ;
+        
+    
